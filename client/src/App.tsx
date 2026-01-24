a566ac92048a7ae32596f371ff6bc6424fea13c5
Alan Gibran <alan@ceosnm.com>
2026-01-24 11:14:00 -0600

    fix: add react-helmet-async for SEO meta tags

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Switch, Route, useLocation } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { HelmetProvider } from "react-helmet-async";
 import { Toaster } from "@/components/ui/toaster";
 import NotFound from "@/pages/not-found";
 import HomePage from "@/pages/HomePage";
@@ -165,11 +166,13 @@ function Router() {
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Router />
-      <Toaster />
-      <ElevenLabsWidget />
-    </QueryClientProvider>
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <Router />
+        <Toaster />
+        <ElevenLabsWidget />
+      </QueryClientProvider>
+    </HelmetProvider>
   );
 }
 
